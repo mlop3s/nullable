fea8d400ecf1b90c509e73aebfca6fda8a7e5011
mlopes <marco.lop3s@gmail.com>
2020-12-09 15:57:01 +0100

    Initial Template

---
+++ NullablePresentation/Program.cs
@@ -0,0 +1,25 @@
+﻿#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullablePresentation
+{
+    public class Prescription
+    {
+        public long Id { get; set; }
+        public string Posology { get; set; }
+        public string Drug { get; set; }
+        public string Agent { get; set; } 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
