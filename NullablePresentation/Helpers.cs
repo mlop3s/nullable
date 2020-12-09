3661b8f4499b8ce3b499ee066d2b617a2f36b8eb
mlopes <marco.lop3s@gmail.com>
2020-12-09 17:02:51 +0100

    nullable package

---
+++ NullablePresentation/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace NullablePresentation
+{
+    public static class Helpers
+    {
+        public static bool IsStringNullOrEmpty([NotNullWhen(true)]string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
